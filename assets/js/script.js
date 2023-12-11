a95b34a12b57ac828a2e89833234d117c5347a2e
schellmts <matheus.emiliano@hotmail.com>
2023-12-11 20:18:57 -0300

    uso das api's

--- assets/js/script.js
+++ assets/js/script.js
@@ -59,6 +59,81 @@ function cepValidate() {
   }
 }
 
+var botao = document.getElementById('btn-busca');
+    botao.addEventListener('click', function(event) {
+      event.preventDefault();
+    });
+
+function consultaEndereco() {
+  let cep = document.querySelector('#cep').value;
+
+  if (cep.length !== 9) {
+    alert('CEP inválido');
+    return;
+  }
+
+  let url = `https://viacep.com.br/ws/${cep}/json/`;
+
+  fetch(url).then(function(response){
+    response.json().then(mostrarEndereco);
+  });
+}
+
+function mostrarEndereco(dados) {
+  let resultado = document.querySelector('#resultado');
+  let estado = document.querySelector('#estado');
+  if (dados.erro) {
+    resultado.innerHTML = `<p class="cidade"> Não foi possível localizar!</p>`;
+    estado.innerHTML = `<p class="cidade"> Não foi possível localizar!</p>`;
+  }
+  else {
+    resultado.innerHTML = `<p class="cidade">${dados.localidade}</p>`
+    estado.innerHTML = `<p class="cidade">${dados.uf}</p>`
+  }
+}
+
+const searchInput = document.querySelector('#search-rest');
+const resultSearch = document.querySelector('#optionsBox');
+
+searchInput.addEventListener('input', async () => {
+  const searchItem = searchInput.value.toLowerCase();
+  
+  if (searchItem.length > 0) {
+    try {
+      const response = await fetch(`http://localhost:3000/restaurantes?nome_like=${searchItem}`);
+      const data = await response.json();
+      
+      resultSearch.innerHTML = '';
+      resultSearch.style.display = "block";
+
+      data.forEach(restaurante => {
+        const nomeRestaurante = restaurante.nome;
+        const restauranteElemento = document.createElement('div');
+        restauranteElemento.textContent = nomeRestaurante;
+        restauranteElemento.classList.add('option-item')
+        resultSearch.appendChild(restauranteElemento);
+      });
+    } catch (error) {
+      console.error('Erro ao buscar os restaurantes:', error);
+    }
+  } else {
+    optionsBox.style.display = 'none';
+  }
+});
+
+document.addEventListener('click', function(event) {
+  if (!optionsBox.contains(event.target) && event.target !== searchInput) {
+    optionsBox.style.display = 'none';
+  }
+})
+
+optionsBox.addEventListener('click', function(event) {
+  if (event.target.classList.contains('option-item')) {
+    searchInput.value = event.target.textContent;
+    optionsBox.style.display = 'none'
+  }
+})
+
 function cityValidate() {
   if(campos[5].value.length < 3) {
     setError(5);
@@ -79,4 +154,6 @@ $(document).ready(function(){
   $(document).ready(function() {
     $('#cep').mask('00000-000');
   });
+
+  
   
